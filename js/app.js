39305c958091dc74acfbc85f7ecb7f80622a8456
Alexander Serebryakov <alex.serebr@gmail.com>
2020-01-30 18:24:35 +0200

    Adds the camera control

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
+import * as THREE from '../three.js/build/three.module.js';
+import { OrbitControls } from '../three.js/examples/jsm/controls/OrbitControls.js';
+
 var example = (function() {
 	"use strict";
 	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 30000);
+	var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 60000);
 	var light = new THREE.AmbientLight(0xFFFFFF);
 	var renderer = new THREE.WebGLRenderer();
 	var box;
 	var coordinates;
 	var rotation = 0;
+	var controls = new OrbitControls( camera, renderer.domElement );
 
 	function initScene() {
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		document.getElementById("wgl-container").appendChild( renderer.domElement );
-		camera.position.set(0, 100, 0);
+		//controls.update() must be called after any manual changes to the camera's transform
+		camera.position.set( 0, 100, 0 );
+		controls.update();
 		scene.add(camera);
 		scene.add(light);
 		read_data();
@@ -49,12 +55,13 @@ var example = (function() {
 	}
 
 	function render() {
-		rotation += 0.001;
-		camera.position.x = 1000 * Math.cos(rotation);
-		camera.position.z = 1000 * Math.sin(rotation);
-		camera.lookAt( new THREE.Vector3(0, 0, 0));
-		renderer.render(scene, camera);
 		requestAnimationFrame(render);
+		controls.update();
+//		rotation += 0.001;
+//		camera.position.x = 1000 * Math.cos(rotation);
+//		camera.position.z = 1000 * Math.sin(rotation);
+//		camera.lookAt( new THREE.Vector3(0, 0, 0));
+		renderer.render(scene, camera);
 	}
 
 	function read_data() {
