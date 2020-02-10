09a48cf1d57f64e024648a685214bc8e1d842d9f
Alexander Serebryakov <alex.serebr@gmail.com>
2020-02-10 13:00:16 +0200

    Reduces the amount of transferred data by removing unnecessary content

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -2,11 +2,10 @@ import urllib.request
 import csv
 
 header = """var data = (function() {
-	function NamedPoint(name,  x,  y,  z) {
+	function P(x,  y,  z) {
 		this.x = x
 		this.y = y
 		this.z = z
-		this.name = name
 	}
 
 	function coordinatesList() {
@@ -26,7 +25,7 @@ def write_data(output):
     with urllib.request.urlopen(link) as csvfile:
          reader = csv.DictReader(csvfile.read().decode('utf-8').splitlines())
          for row in reader:
-             output.write("new NamedPoint(\"{}\", {}, {}, {}),\n".format(row['name'], row['x'], row['y'], row['z']).encode())
+             output.write("new P({},{},{}),\n".format(row['x'], row['y'], row['z']).encode())
 
 with open("../data/data.js", "wb") as out:
     out.write(header.encode())
